da893f935e8a15ae5ed32bd2482504391cb7cc9d
EugBulava <jonbulova97@gmail.com>
2018-01-21 18:04:12 +0300

    fix algorithm decomposition

--- src/main/java/converter/IntegerNumberToStringConverter.java
+++ src/main/java/converter/IntegerNumberToStringConverter.java
@@ -22,7 +22,7 @@ public class IntegerNumberToStringConverter implements NumberConverter<String> {
     private BigInteger amount;
 
     //forms of type number
-    String[][] forms;
+    private String[][] forms;
 
     private static final String FILE_PATH = "src/main/resources/referenceBook.txt";
 
@@ -37,32 +37,13 @@ public class IntegerNumberToStringConverter implements NumberConverter<String> {
         this.forms = readFormsFromFile();
     }
 
-    public static String morph(long n, String from1, String from2, String from5) {
-
-        n = Math.abs(n) % 100;
-        long n1 = n % 10;
-        if (n > 10 && n < 20) return from5;
-        if (n1 > 1 && n1 < 5) return from2;
-        if (n1 == 1) return from1;
-        return from5;
-    }
-
     public String convert() throws NumberLengthException {
-        //kinds of gender
-        String[][] kinds = {
-                {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},
-                {"", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},
-        };
-
-        String[] str100 = {"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
-        String[] str11 = {"", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать", "двадцать"};
-        String[] str10 = {"", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"};
 
         BigInteger number = amount;
 
         //check that the number entered is less than the types in the dictionary
         String result = "";
-        if(amount.toString().length()>forms.length*3)
+        if(amount.toString().length() > forms.length*3)
             throw new NumberLengthException("add to reference book more examples");
 
         //check for zero
@@ -75,64 +56,100 @@ public class IntegerNumberToStringConverter implements NumberConverter<String> {
                 number = number.abs();
             }
 
-            BigInteger tempNumber = number;
+            ArrayList segments = divideIntoSegments(number);
+            result = segmentsToString(segments);
 
-            ArrayList segments = new ArrayList();
-            // segmented number and add to list
-            while (tempNumber.compareTo(new BigInteger("999")) == 1) {
-                BigInteger segment = tempNumber.divide(new BigInteger("1000"));
-                segments.add(tempNumber.add(segment.multiply(new BigInteger("1000")).negate()));
-                tempNumber = segment;
-            }
-
-            segments.add(tempNumber);
-            Collections.reverse(segments);
-
-            int level = segments.size() - 1;
-
-
-            for (int i = 0; i < segments.size(); i++) {
-                int kind = Integer.valueOf(forms[level][3].toString()); //check gender
-                int currentSegment = Integer.valueOf(segments.get(i).toString()); //current segment
-
-                if (currentSegment == 0 && level >= 1) {
-                    level--;
-                    continue;
-                }
-
-                String segmentString = String.valueOf(currentSegment);
-
-                if (segmentString.length() == 1) segmentString = "00" + segmentString; // check length of segment and add "0"
-                if (segmentString.length() == 2) segmentString = "0" + segmentString; // or "00"
-
-                int r1 = Integer.valueOf(segmentString.substring(0, 1)); // divide the segment 1**
-                int r2 = Integer.valueOf(segmentString.substring(1, 2)); // *2*
-                int r3 = Integer.valueOf(segmentString.substring(2, 3)); // **3
-                int r23 = Integer.valueOf(segmentString.substring(1, 3));// *23
-
-                    if (currentSegment > 99) result += str100[r1] + " ";
-
-                if (r23 > 20) {
-                    result += str10[r2] + " ";
-                    if(!"".equals(kinds[kind][r3])) {
-                        result += kinds[kind][r3] + " ";
-                    }
-                } else {
-                        if (r23 > 9) result += str11[r23 - 9] + " ";
-
-                    else
-                        result += kinds[kind][r3] + " ";
-                }
-                result += morph(currentSegment, forms[level][0], forms[level][1], forms[level][2]) + " ";
-                level--;
-            }
-            //result
+            //process result
             result = result.substring(0, result.length()-2);
             if(result.endsWith(" ")){
                 result = result.substring(0, result.length()-1);
             }
         }
+
         return result;
+    }
+
+    private String segmentsToString(ArrayList segments) {
+
+        String result = "";
+
+        //kinds of gender
+        String[][] kinds = {
+                {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},
+                {"", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"},
+        };
+
+        String[] str100 = {"", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
+        String[] str11 = {"", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать", "двадцать"};
+        String[] str10 = {"", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"};
+
+        int level = segments.size() - 1;
+
+
+        for (int i = 0; i < segments.size(); i++) {
+            int kind = Integer.valueOf(forms[level][3].toString()); //check gender
+            int currentSegment = Integer.valueOf(segments.get(i).toString()); //current segment
+
+            if (currentSegment == 0 && level >= 1) {
+                level--;
+                continue;
+            }
+
+            String segmentString = String.valueOf(currentSegment);
+
+            if (segmentString.length() == 1) segmentString = "00" + segmentString; // check length of segment and add "0"
+            if (segmentString.length() == 2) segmentString = "0" + segmentString; // or "00"
+
+            int r1 = Integer.valueOf(segmentString.substring(0, 1)); // divide the segment 1**
+            int r2 = Integer.valueOf(segmentString.substring(1, 2)); // *2*
+            int r3 = Integer.valueOf(segmentString.substring(2, 3)); // **3
+            int r23 = Integer.valueOf(segmentString.substring(1, 3));// *23
+
+            if (currentSegment > 99) result += str100[r1] + " ";
+
+            if (r23 > 20) {
+                result += str10[r2] + " ";
+                if(!"".equals(kinds[kind][r3])) {
+                    result += kinds[kind][r3] + " ";
+                }
+            } else {
+                if (r23 > 9) {
+                    result += str11[r23 - 9] + " ";
+                } else {
+                    result += kinds[kind][r3] + " ";
+                }
+            }
+            result += morph(currentSegment, forms[level][0], forms[level][1], forms[level][2]) + " ";
+            level--;
+        }
+
+        return result;
+    }
+
+    private String morph(long n, String from1, String from2, String from5) {
+
+        n = Math.abs(n) % 100;
+        long n1 = n % 10;
+        if (n > 10 && n < 20) return from5;
+        if (n1 > 1 && n1 < 5) return from2;
+        if (n1 == 1) return from1;
+        return from5;
+    }
+
+    private ArrayList divideIntoSegments(BigInteger number){
+
+        ArrayList segments = new ArrayList();
+
+        while (number.compareTo(new BigInteger("999")) == 1) {
+            BigInteger segment = number.divide(new BigInteger("1000"));
+            segments.add(number.add(segment.multiply(new BigInteger("1000")).negate()));
+            number = segment;
+        }
+
+        segments.add(number);
+        Collections.reverse(segments);
+
+        return segments;
     }
 
     private String[][] readFormsFromFile(){
